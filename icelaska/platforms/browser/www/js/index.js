3c5cd349578515164556689cb322245912209fe8
Vishnu Ravi <vishnu@vishnu.io>
2016-04-24 02:33:45 -0400

    Added weather data

--- icelaska/platforms/browser/www/js/index.js
+++ icelaska/platforms/browser/www/js/index.js
@@ -7,7 +7,8 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     onDeviceReady: function() {
-        navigator.geolocation.getCurrentPosition(locationDetected, geolocationError);
+        navigator.geolocation.getCurrentPosition(locationDetected);
+        var watchId = navigator.geolocation.watchPosition(locationUpdated);
         shake.startWatch(onShake);
     }
 };
@@ -15,17 +16,46 @@ var app = {
 app.initialize();
 
 var locationDetected = function(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    var heading = position.coords.heading;
-    $('#latitude').text(latitude);
-    $('#longitude').text(longitude);
+    updateCoords(position);
+    getWeather(latitude, longitude);
 };
 
-function geolocationError(error) {
-    alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
-}
+var locationUpdated = function(position){
+    updateCoords(position);
+};
 
 var onShake = function () {
   console.log("Shake detected.");
 };
+
+function getWeather(latitude, longitude){
+    console.log('Fetching weather...');
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=e00d6166d5a2cc7acd31371ca9c2b0f9",function(result){
+    $('#temp').text(KtoF(result.main.temp) + "F");
+    $('#temp_min').text(KtoF(result.main.temp_min) + "F");
+    $('#temp_max').text(KtoF(result.main.temp_max) + "F");
+    $('#condition').text(result.weather[0].description);
+    $('#pressure').text(result.main.pressure + "hPa");
+    $('#visibility').text(result.visibility);
+    $('#wind_speed').text(result.wind.speed);
+    $('#wind_deg').text(result.wind.deg);
+    $('#sunrise').text(epochToJsDate(result.sys.sunrise));
+    $('#sunset').text(epochToJsDate(results.sys.sunset));
+    });
+}
+
+function updateCoords(position){
+    console.log('Updating Coordinates...');
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude
+    $('#latitude').text(latitude);
+    $('#longitude').text(longitude);
+}
+
+function KtoF(k){
+    return Math.round((9/5)*(k-273)+32);
+}
+
+function epochToJsDate(ts){
+    return new Date(ts*1000);
+}
